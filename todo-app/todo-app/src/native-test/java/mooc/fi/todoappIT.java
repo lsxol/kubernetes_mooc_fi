bb84543e967d62eb34764c5c0bc589251c474343
Daniel Kaźmierczak <daniel.kazmierczak@nekken.pl>
2026-07-22 18:30:57 +0200

    todo-app init

---
+++ todo-app/todo-app/src/native-test/java/mooc/fi/todoappIT.java
@@ -0,0 +1,8 @@
+package mooc.fi;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class todoappIT extends todoappTest {
+    // Execute the same tests but in packaged mode.
+}
