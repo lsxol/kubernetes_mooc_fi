0a6c34594f54b5c79d1175dc7f05679481bf9398
Daniel Kaźmierczak <daniel.kazmierczak@nekken.pl>
2026-07-30 13:17:19 +0200

    1.12. The project, step 6

--- todo-app/todo-app/src/main/java/mooc/fi/todoapp.java
+++ todo-app/todo-app/src/main/java/mooc/fi/todoapp.java
@@ -1,16 +1,51 @@
 package mooc.fi;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.time.Instant;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("")
 public class todoapp {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/image")
+    @Produces("image/jpg")
+    public byte[] getImage() {
+        java.nio.file.Path file = java.nio.file.Path.of("/usr/src/app/files/image.jpg");
+        try {
+            boolean fresh = Files.exists(file) && Files.getLastModifiedTime(file, LinkOption.NOFOLLOW_LINKS).toInstant()
+                    .isAfter(Instant.now().minusSeconds(600));
+            if (!fresh) {
+                HttpClient client = HttpClient.newBuilder()
+                        .followRedirects(HttpClient.Redirect.NORMAL)
+                        .build();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(java.net.URI.create("https://picsum.photos/1200"))
+                        .build();
+                byte[] imageBytes = client.send(request, HttpResponse.BodyHandlers.ofByteArray()).body();
+                Files.write(file, imageBytes);
+                return imageBytes;
+            } else {
+                return Files.readAllBytes(file);
+            }
+        } catch (java.io.IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 }
